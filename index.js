2eb0f2cd4a1a8f40078a1d7608fe22b469b6092a
pleasant-calculator-7755 <domcode88@gmail.com>
2020-09-01 02:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function takeANumber(line, name){
 }
 
 function nowServing(line){
- for (let i = 1; i < line.length; i +=1)
+ for (let i = 1; i < line.length; i +=1){
+  return line[0];
+ }
     
 }
